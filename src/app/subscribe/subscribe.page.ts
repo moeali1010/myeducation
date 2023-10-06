59ff27c3a94ac80e30676bb0e7e0931479210d8f
Mohamed Ali <workmail1010@gmail.com>
2023-10-06 15:04:00 +0200

    subscribe done

--- src/app/subscribe/subscribe.page.ts
+++ src/app/subscribe/subscribe.page.ts
@@ -12,13 +12,13 @@ export class SubscribePage implements OnInit {
   pastYears: number[] = [];
   months: string[] = [];
   days: number[] = [];
-
+  monthLimit: number = 0;
   constructor() {}
 
   ngOnInit() {
     this.displayLast20years();
     this.displayMonths();
-    this.displayDays();
+    this.displayDays(this.currentYear, 'January');
   }
 
   // topics
@@ -55,8 +55,49 @@ export class SubscribePage implements OnInit {
     ];
   }
 
-  displayDays() {
-    for (let index = 1; index < 32; index++) {
+  // check if year is leab
+  // isLeapYear = (year: number): boolean => {
+  //   return new Date(year, 1, 29).getDate() === 29;
+  // };
+  leapYear(year: number) {
+    return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+  }
+
+  displayDays(year: any, month: any) {
+    if (this.leapYear(year) && month == 'February') {
+      // leapYear
+      this.monthLimit = 30;
+    }
+    if (!this.leapYear(year) && month == 'February') {
+      // not leapYear
+      this.monthLimit = 29;
+    }
+
+    // 31 days months
+    if (
+      month == 'January' ||
+      month == 'March' ||
+      month == 'May' ||
+      month == 'July' ||
+      month == 'August' ||
+      month == 'October' ||
+      month == 'December'
+    ) {
+      this.monthLimit = 32;
+    }
+
+    // 30 days months
+    if (
+      month == 'April' ||
+      month == 'June' ||
+      month == 'September' ||
+      month == 'November'
+    ) {
+      this.monthLimit = 31;
+    }
+
+    this.days = [];
+    for (let index = 1; index < this.monthLimit; index++) {
       this.days.push(index);
     }
   }
