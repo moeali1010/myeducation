89d3b096fd5b0d98a9e69d6623b4f005e4bcc0b5
Mohamed Ali <workmail1010@gmail.com>
2023-10-06 20:29:14 +0200

    form values with validation

--- src/app/subscribe/subscribe.module.ts
+++ src/app/subscribe/subscribe.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -12,9 +12,10 @@ import { SubscribePage } from './subscribe.page';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    SubscribePageRoutingModule
+    SubscribePageRoutingModule,
   ],
-  declarations: [SubscribePage]
+  declarations: [SubscribePage],
 })
 export class SubscribePageModule {}
--- src/app/subscribe/subscribe.page.ts
+++ src/app/subscribe/subscribe.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-subscribe',
   templateUrl: './subscribe.page.html',
@@ -7,18 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubscribePage implements OnInit {
   topics: any = [];
+  form: any;
   // date
   currentYear: number = new Date().getFullYear();
   pastYears: number[] = [];
   months: string[] = [];
   days: number[] = [];
   monthLimit: number = 0;
-  constructor() {}
+  constructor(
+    public formBuilder: FormBuilder,
+    private alertController: AlertController
+  ) {}
 
   ngOnInit() {
+    this.initializeForm();
     this.displayLast20years();
     this.displayMonths();
     this.displayDays(this.currentYear, 'January');
+  }
+
+  initializeForm() {
+    this.form = this.formBuilder.group({
+      contact_name: new FormControl(null, [Validators.required]),
+      contact_email: new FormControl(null, [
+        Validators.required,
+        Validators.email,
+      ]),
+      child_name: new FormControl(null, [Validators.required]),
+      child_dob_day: new FormControl('1', [Validators.required]),
+      child_dob_month: new FormControl('January', [Validators.required]),
+      child_dob_year: new FormControl(this.currentYear, [Validators.required]),
+      child_grade: new FormControl(null, [Validators.required]),
+      child_gender: new FormControl(null, [Validators.required]),
+    });
   }
 
   // topics
@@ -56,9 +78,6 @@ export class SubscribePage implements OnInit {
   }
 
   // check if year is leab
-  // isLeapYear = (year: number): boolean => {
-  //   return new Date(year, 1, 29).getDate() === 29;
-  // };
   leapYear(year: number) {
     return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
   }
@@ -100,5 +119,31 @@ export class SubscribePage implements OnInit {
     for (let index = 1; index < this.monthLimit; index++) {
       this.days.push(index);
     }
+  }
+
+  async presentErrorAlert() {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      subHeader:
+        'please fill all fields correctly , then press continue to payment',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+  async presentSuccessAlert() {
+    const alert = await this.alertController.create({
+      header: 'Success',
+      subHeader: 'Thanks you , we will contact you soon',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+  submitForm() {
+    !this.form.valid ? this.presentErrorAlert() : this.presentSuccessAlert();
+
+    console.log('this.form', this.form.value);
+    console.log(' this.topics', this.topics);
   }
 }
