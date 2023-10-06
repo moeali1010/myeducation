8bc44c59a0fdec3af583fad31df9dd8cf14f5169
Mohamed Ali <workmail1010@gmail.com>
2023-10-06 03:22:33 +0200

    topics

--- src/app/subscribe/subscribe.page.ts
+++ src/app/subscribe/subscribe.page.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subscribe.page.scss'],
 })
 export class SubscribePage implements OnInit {
+  topics: any = [];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  // topics
+  addToTopics(topic: string) {
+    // if exist in array remove it
+    if (this.topics.includes(topic)) {
+      this.topics.splice(this.topics.indexOf(topic), 1);
+      return;
+    }
+    // if length of array less than 3 items
+    this.topics.length < 3 ? this.topics.push(topic) : '';
   }
-
 }
